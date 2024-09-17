c9a4f5a3d714f14f44c007b5c47a7de306f55b37
Nicholas Wolf <100742628+wolfuoa@users.noreply.github.com>
2024-09-17 14:19:41 +1200

    feat: :sparkles: GYroscope readings

--- src/COMSYS704/Projects/STM32L476JG-SensorTile/Applications/ALLMEMS1/Src/main.c
+++ src/COMSYS704/Projects/STM32L476JG-SensorTile/Applications/ALLMEMS1/Src/main.c
@@ -310,6 +310,52 @@ static void startAcc()
 	BSP_LSM303AGR_WriteReg_Acc(0x20U, entry, 1);
 }
 
+static void readGyroscope()
+{
+	uint8_t outx_l;
+	uint8_t outx_h;
+	uint8_t outy_l;
+	uint8_t outy_h;
+	uint8_t outz_l;
+	uint8_t outz_h;
+
+	int16_t outx;
+	int16_t outy;
+	int16_t outz;
+
+	BSP_LSM6DSM_ReadReg_Gyro(0x22U, &outx_l, 1);
+	BSP_LSM6DSM_ReadReg_Gyro(0x23U, &outx_h, 1);
+	BSP_LSM6DSM_ReadReg_Gyro(0x24U, &outy_l, 1);
+	BSP_LSM6DSM_ReadReg_Gyro(0x25U, &outy_h, 1);
+	BSP_LSM6DSM_ReadReg_Gyro(0x26U, &outz_l, 1);
+	BSP_LSM6DSM_ReadReg_Gyro(0x27U, &outz_h, 1);
+
+	outx = (outx_h << 7);
+	outx |= outx_l;
+
+	outy = (outy_h << 7);
+	outy |= outy_l;
+
+	outz = (outz_h << 7);
+	outz |= outz_l;
+
+	int negative;
+
+	negative = (outx_h >> 7);
+	if(negative)
+		outx = (outx | ~((1 << 15) -1));
+
+	negative = (outy_h >> 7);
+	if(negative)
+		outy = (outy | ~((1 << 15) -1));
+
+	negative = (outz_h >> 7);
+	if(negative)
+		outz = (outz | ~((1 << 15) -1));
+
+	XPRINTF("GYRO=%d,%d,%d\r\n", outx, outy, outz);
+}
+
 static void readMag()
 {
 	uint8_t outx_l;
@@ -432,6 +478,8 @@ int main(void)
 
 	startMag();
 	startAcc();
+	startGyroscope();
+
 
 	uint8_t BufferToWrite[10] = "ABCDE";
 	//***************************************************
@@ -481,6 +529,7 @@ int main(void)
 			//*********get sensor data**********
 			readMag();
 			readAcc();
+			readGyroscope();
 
 			//*********process sensor data*********
 
